e7f2a1956776ea53da4dcd2603952be0f71f1605
OfentseXI <dofentse69@gmail.com>
2024-04-22 11:47:08 +0200

    initial data import

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TASK: import helper functions from utils
 // TASK: import initialData
 
+import { initialData } from "./initialData";
 
 /*************************************************************************************************************************************************
  * FIX BUGS!!!
